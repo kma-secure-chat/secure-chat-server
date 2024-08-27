935f33f301ddc99bb12e2488234137d438df1500
Ta Minh Huy <huytm2k3@gmail.com>
2024-08-27 16:02:22 +0700

    init: implement express, socket.io

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('an user connected');
+
+    // Handle a user sending a message
+    socket.on('chat message', (msg) => {
+        console.log(msg);
+        let formattedMsg = {
+            id: 10,
+            text: msg,
+            sent_at: "2024-08-23T09:19:01.333Z",
+            user: {
+                id: 4,
+                status: 'away',
+                name: "Jane Smith",
+                avatar: "https://randomuser.me/api/portraits"
+            }
+        }
+
+        io.emit('chat message', formattedMsg);
+    });
+
+    // Handle a user disconnecting
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
