8675c30b5535d53315d3a4eaae4d9ca49f7f3046
Ta Minh Huy <huytm2k3@gmail.com>
2024-09-22 14:47:05 +0700

    chore: change port to 5678

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-const PORT = 3000;
+const PORT = 5678;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
