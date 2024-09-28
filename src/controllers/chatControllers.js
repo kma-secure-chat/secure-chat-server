2afec8395ab61ea80a3f7a84231cca195d08bd8b
Ta Minh Huy <huytm2k3@gmail.com>
2024-09-28 15:07:54 +0700

    minor update

--- src/controllers/chatControllers.js
+++ src/controllers/chatControllers.js
@@ -1,5 +1,21 @@
 const pool = require("../config/pool");
 
+exports.findConversation = async (req, res) => {
+    const { receiver_id } = req.params;
+
+    pool.query('SELECT * FROM conversations WHERE (user_one_id = $1 AND user_two_id = $2) OR (user_one_id = $2 AND user_two_id = $1)', [req.user.id, receiver_id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+
+        if (results.rows.length > 0) {
+            res.status(200).send(results.rows[0]);
+        } else {
+            res.status(200).send(null);
+        }
+    });
+};
+
 exports.getConversations = async (req, res) => {
     pool.query(`
             SELECT
@@ -30,6 +46,43 @@ exports.getConversations = async (req, res) => {
         res.status(200).send(results.rows);
     });
 }
+
+exports.getConversation = async (req, res) => {
+    const { conversation_id } = req.params;
+
+    pool.query(`
+        SELECT
+            c.id,
+                CASE WHEN c.user_one_id = $1 THEN u2.fullname ELSE u1.fullname
+                END AS name,
+                c.user_one_id,
+                c.user_two_id,
+                m.content as last_message,
+                m.created_at as last_message_time
+            FROM
+                conversations c
+            JOIN
+                users u1 ON u1.id = c.user_one_id
+            JOIN 
+                users u2 ON u2.id = c.user_two_id
+        LEFT JOIN
+            (SELECT id, conversation_id, content, created_at
+            FROM messages
+            ORDER BY created_at DESC
+            LIMIT 1) m ON m.conversation_id = c.id
+        WHERE
+            c.id = $1
+    `, [conversation_id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        if (results.rows.length > 0) {
+            res.status(200).send(results.rows[0]);
+        } else {
+            res.status(404).send({ message: 'Conversation not found' });
+        }
+    });
+};
 
 exports.createNewConversations = async (req, res) => {
     const { user_two_id } = req.body;
--- src/routes/chatRoutes.js
+++ src/routes/chatRoutes.js
@@ -1,9 +1,11 @@
-const { createNewConversations, sendMessage, getConversations, getMessages } = require("../controllers/chatControllers");
+const { createNewConversations, sendMessage, getConversations, getMessages, findConversation, getConversation } = require("../controllers/chatControllers");
 const { verifyJwt } = require('../middleware/verifyJwt');
 
 module.exports = (app, upload) => {
     app.get('/api/conversations', verifyJwt, getConversations);
+    app.get('/api/conversation/:conversation_id', getConversation);
     app.post('/api/conversation/new', verifyJwt, createNewConversations);
+    app.get('/api/conversation/find/:receiver_id', verifyJwt, findConversation);
 
     app.get('/api/messages/', verifyJwt, getMessages);
     app.post('/api/messages/send', verifyJwt, sendMessage);
